789dcb57594f09f86bef4e5908c5d8b5c0696f62
Munawwar <munawwarfiroz@hotmail.com>
2024-04-15 23:54:16 +0400

    optimize html tagged template literal util

--- src/html.js
+++ src/html.js
@@ -5,23 +5,24 @@ const characterEntitiesMapping = {
   "'": '&apos;',
   '"': '&quot;',
 };
-function escape(text) {
-  if (!text) return text;
-  return text.replace(/[<>&'"]/g, character => characterEntitiesMapping[character]);
-}
+const findRegex = /[<>&'"]/g;
+const replaceFunc = character => characterEntitiesMapping[character];
+
 // Utility for users not using a template library
 function html(strings, ...values) {
-  return strings.reduce((acc, string, index) => {
+  let acc = strings[0];
+  for (let index = 1; index < strings.length; index++) {
     let value = String(values[index - 1]);
-    if ((strings[index - 1] || '').endsWith("$")) {
+    if (strings[index - 1].endsWith("$")) {
       // If $ sign precedes the interpolation, then its considered safe to
       // add the unescaped / raw HTML
       acc = acc.slice(0, -1);
-    } else {
-      value = escape(value);
+    } else if (value) {
+      value = value.replace(findRegex, replaceFunc);
     }
-    return acc + value + string;
-  });
+    acc += value + strings[index];
+  }
+  return acc;
 }
 
 export { html };
